18e82b7110205d38835e64f87bb34ed80d2e8e27
Oliver Jones <oapjones@gmail.com>
2014-08-23 23:03:49 +0700

    Configured app for Production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # http://bloccit.herokuapp.com
+   config.action_mailer.default_url_options = { host: 'ojbloccit.herokuapp.com/' }
 end
