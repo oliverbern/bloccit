1071cc35ac055e4456a596f4b9bdc60360787925
Oliver Jones <oapjones@gmail.com>
2014-08-23 22:52:34 +0700

    delete post2

--- db/migrate/20140822123802_add_user_to_posts2.rb
+++
@@ -1,8 +0,0 @@
-class AddUserToPosts < ActiveRecord::Migration
-  def change
-    add_column :posts, :user_id, :integer
-      add_index :posts, :user_id
-
-      params.permit(:user_id)
-  end
-end
