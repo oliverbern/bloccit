e3472c466250a4ff6daa9d7d177c5d66984f4459
oliverbern <oapjones@gmail.com>
2014-09-26 23:21:41 +0200

    added avatar functin

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,52 @@
+class TopicsController < ApplicationController
+  def index
+     @topics = Topic.all
+     authorize @topics
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+     @posts = @topic.posts
+    authorize @topic
+  end
+
+  def new
+    @topic = Topic.new
+    authorize @topic
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+    authorize @topic
+    if @topic.save
+      flash[:notice] = "Topic was saved."
+      redirect_to @topic
+    else
+      flash[:error] = "There was an error saving the topic. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
+    authorize @topic
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+    authorize @topic
+    if @topic.update_attributes(topic_params)
+      redirect_to @topic
+    else 
+      flash[:error] = "There was an error saving the topic. Please try again."
+      render :edit
+  end
+end
+
+private
+
+def topic_params
+  params.require(:topic).permit(:name, :description, :public)
+end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,84 @@
+require 'faker'
+
+# Create Users
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
+#Crate Topics
+15.times do
+  Topic.create!(
+    name: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph
+    )
+end
+topics = Topic.all
+# Note: by calling `User.new` instead of `create`,
+# we create an instance of User which isn't immediately saved to the database.
+
+# The `skip_confirmation!` method sets the `confirmed_at` attribute
+# to avoid triggering an confirmation email when the User is saved.
+
+# The `save` method then saves this User to the database.
+
+
+# Create Posts
+50.times do
+  Post.create!(
+    user:   User.all.sample,
+      topic: Topic.all.sample,
+    title:  Faker::Lorem.sentence,
+    body:   Faker::Lorem.paragraph
+  )
+end
+posts = Post.all
+
+# Create Comments
+100.times do
+  Comment.create!(
+    #user: users.sample,   # we have not yet associated Users with Comments
+    post: posts.sample,
+    body: Faker::Lorem.paragraph
+  )
+end
+
+ # Create an admin user
+ admin = User.new(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ admin.skip_confirmation!
+ admin.save!
+ 
+ # Create a moderator
+ moderator = User.new(
+   name:     'Moderator User',
+   email:    'moderator@example.com', 
+   password: 'helloworld',
+   role:     'moderator'
+ )
+ moderator.skip_confirmation!
+ moderator.save!
+ 
+ # Create a member
+ member = User.new(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld',
+ )
+ member.skip_confirmation!
+ member.save!
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
