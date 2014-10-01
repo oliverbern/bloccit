6821a33d26a23d0e946b341229f9bee8e3124ca1
oliverbern <oapjones@gmail.com>
2014-10-01 17:00:42 +0200

    paginatino

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,13 +1,12 @@
 class TopicsController < ApplicationController
   def index
-     @topics = Topic.all
+     @topics = Topic.paginate(page: params[:page], per_page: 5)
      authorize @topics
   end
 
   def show
     @topic = Topic.find(params[:id])
-     @posts = @topic.posts
-    authorize @topic
+     @posts = @topic.posts.paginate(page: params[:page], per_page: 5)
   end
 
   def new
