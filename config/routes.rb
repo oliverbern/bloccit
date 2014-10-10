c4ec09a868d51b7d8f4f3ce7dfb02f0a550f75ef
oliverbern <oapjones@gmail.com>
2014-10-10 16:20:28 +0200

    public profiles

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Bloccit::Application.routes.draw do
   get 'topics/edit'
 
   devise_for :users
-    resources :users, only: [:update]
-  
+    resources :users
+    
   resources :topics do
       resources :posts, except: [:index]
     end
